ab65fa48553d30037fd0d8520ae4e8e5722aae9f
SPal-14 <kolkatasouvik1@gmail.com>
2023-11-15 05:36:51 +0530

    Version 1.0

--- app/main.py
+++ app/main.py
@@ -1,64 +1,51 @@
-# from fastapi import FastAPI, File, UploadFile
-# from pydantic import BaseModel
-# from app.model.model import predict_pipeline
-# from app.model.model import __version__ as model_version
-
-
-# app = FastAPI()
-
-
-# class AudioIn(BaseModel):
-#     wav: UploadFile = File(...)
-
-    
-# class PredictionOut(BaseModel):
-#     emotion: str
-
-    
-# @app.get("/")
-# def home():
-#     return {"health_check": "OK", "model_version": model_version}
-
-
-
-# @app.post("/predict", response_model=PredictionOut)
-# def predict(payload: UploadFile = File(...)):
-#     emotions = predict_pipeline(payload)
-#     return PredictionOut(emotion=emotions[0])
 from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+import json
 import requests
 
 app = FastAPI()
 
-# Replace with your Deepgram API Key
-api_key = "313178ad7ceb92d4d389947590d93ae083bc743b"
+# Replace with the new Deepgram API Key
+api_key = "9458e94f9a1a422f6e3d87e575ccbd80f59fdb46"
 
-# Specify the Deepgram API endpoint
+# Specifying the Deepgram API endpoint
 api_url = "https://api.deepgram.com/v1/listen"
 
 
-def get_deepgram_headers():
-    return {"Authorization": f"Bearer {api_key}"}
-
-
 @app.post("/transcribe/")
-async def transcribe_audio(file: UploadFile = File(...), headers: dict = Depends(get_deepgram_headers)):
+async def transcribe_audio(file: UploadFile = File(..., upload_max_size=50_000_000)):
     try:
         # Prepare the payload with the audio file
         files = {"content": (file.filename, file.file, file.content_type)}
 
+        # # Specify Deepgram parameters
+        # deepgram_params = {
+        #     "model": "nova-2",
+        #     "language": "en",
+        #     "smart_format": True,
+        # }
+
+        # Specify Deepgram headers
+        headers = {
+             "accept": "application/json",
+             "content-type": "application/json",
+             "Authorization": f"Token {api_key}"
+            }
+        # Prepare the payload for Deepgram API
+        payload = { "url": f"https://static.deepgram.com/examples/{file.filename}" }
+
         # Send the POST request to Deepgram API for transcription
-        response = requests.post(api_url, headers=headers, files=files)
+        response = requests.post(api_url, headers=headers, json=payload)
 
         # Check if the request was successful (HTTP status code 200)
-        if response.status_code == 200:
-            result = response.json()
-            # Extract the transcript
-            transcript = result["text"]
-            return {"transcript": transcript}
-        else:
-            raise HTTPException(status_code=response.status_code, detail=f"Deepgram API Error: {response.text}")
+        response.raise_for_status()
 
+        # Extract the transcript
+        result = response.json()
+        transcript = result.get("results", {}).get("channels", [{}])[0].get("alternatives", [{}])[0].get("transcript", "")
+
+        return {"transcript": transcript}
+
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Request to Deepgram API failed: {str(e)}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
-
